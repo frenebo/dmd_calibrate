06a46c2186bbef4ec0a1e0f88cf7a48046fb808f
Paul Kreymborg <frenebo@gmail.com>
2022-05-19 15:22:08 -0700

    do black white thing

--- code/calibrate.py
+++ code/calibrate.py
@@ -1,5 +1,5 @@
 import numpy as np
-from time import sleep
+import time
 import cv2
 from PIL import Image
 import subprocess
@@ -15,7 +15,21 @@ class Calibrator:
         self.running_feh = False
 
     def calibrate(self):
-        self.map_grid()
+        while True:
+            print("Black")
+            # black_dmd_img = np.zeros((height,width),dtype=np.uint8)
+            self.display_image(black_dmd_img)
+
+            time.sleep(5)
+            # background_black = self.capture_blue_pixels()
+            # self.save_bw_floats(background_black,"black.jpg")
+
+            print("White")
+            self.display_image(np.ones_like(black_dmd_img)*np.iinfo(np.uint8).max)
+
+            time.sleep(5)
+            # background_white = self.capture_blue_pixels()
+            # self.save_bw_floats(background_white, "white.jpg")
 
     def show_circle_at(self,x,y):
         dmd_w, dmd_h = self.dmd_display_dims
