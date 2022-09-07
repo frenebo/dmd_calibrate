46b301dc4f36249fd719e6c6de8c13e0a0125db3
Paul Kreymborg <frenebo@gmail.com>
2022-09-07 14:36:54 -0700

    figuring out calibration

--- desktop/src/calibrate.py
+++ desktop/src/calibrate.py
@@ -8,7 +8,12 @@ import os
 from .raspiinterface import RaspiController
 from .camera import take_micromanager_pic
 
-DMD_H_W = (800, 1280)
+DMD_H_W = (912, 1140)
+
+# When calibration doesn't work right
+class CalibrationException(Exception):
+    pass
+
 
 def save_img_for_dmd(np_float_img, path):
     assert np_float_img.dtype == float, "Image should be float array"
@@ -23,37 +28,160 @@ def save_img_for_dmd(np_float_img, path):
 
     tifffile.imwrite(path, np_int8_img)
 
-def calibrate(core, raspi_controller, workdir):
-    dmd_img_dir = os.path.join(workdir, "imgsfordmd")
-    os.makedirs(dmd_img_dir, exist_ok=True)
+
+def draw_white_circle(np_float_img, diameter, center_x, center_y):
+    assert np_float_img.dtype == float, "Image should be float array"
+    assert int(diameter) == diameter, "Diameter value should be integer"
+    assert int(center_x) == center_x, "Center x value should be integer"
+    assert int(center_y) == center_y, "Center y value should be integer"
+    assert diameter > 0, "Diameter should be positive"
+    assert diameter % 2 == 1, "Diameter should be odd, so the circle will be centered on a single pixel"
+
+    diameter = int(diameter)
+    center_x = int(center_x)
+    center_y = int(center_y)
+
+    circ_xx, circ_yy = np.meshgrid(np.arange(diameter), np.arange(diameter))
+
+    # This should already be a whole number, but casting to an integer so we can array index with it
+    rad = int((diameter - 1) / 2)
+
+    circ_mask = (np.square(circ_xx - rad) + np.square(circ_yy - rad)) <= np.square(rad + 0.5)
+
+    # Convert to float
+    circ_mask *= 1.0
+
+    # Add white circle to brightness values
+    np_float_img[center_x - rad:center_x + rad + 1, center_y - rad:center_y + rad + 1] += circ_mask
+
+    # If the spot wasn't already white, then it'll have brightness more than 1.0 now - fix this here
+    np_float_img[np_float_img > 1.0] = 1.0
+
+
+def get_background_and_white_images(core, raspi_controller, workdir):
+    # @TODO calibrate these values
+    black_samples = 10
+    white_samples = 10
+
+    max_relative_variance_rel_to_b_w_diff = 0.1
+    max_black_field_var_rel_to_b_w_diff = 0.1
+    max_white_field_var_rel_to_b_w_diff = 0.1
 
     black_img_dmd = np.zeros(DMD_H_W, dtype=float)
     black_img_dmd_path = os.path.join(dmd_img_dir, "allblack.tiff")
     save_img_for_dmd(black_img_dmd, black_img_dmd_path)
     raspi_controller.send_and_show_image_on_dmd(black_img_dmd_path)
 
-    black_img_microphoto = take_micromanager_pic(core)
+    black_photos = []
+
+    for i in range(black_samples):
+        black_img_microphoto = take_micromanager_pic_as_float_arr(core)
+        black_photos.append(black_img_microphoto)
+
+
+
+    # avg_black = np.average()
+
 
     white_img_dmd = np.ones(DMD_H_W, dtype=float)
     white_img_dmd_path = os.path.join(dmd_img_dir, "allwhite.tiff")
     save_img_for_dmd(white_img_dmd, white_img_dmd_path)
     raspi_controller.send_and_show_image_on_dmd(white_img_dmd_path)
 
-    white_img_microphoto = take_micromanager_pic(core)
+    white_photos = []
+    for i in range(white_samples):
+        white_img_microphoto = take_micromanager_pic_as_float_arr(core)
+        white_photos.append(white_img_microphoto)
+
+    all_black_samples = np.array(black_photos)
+    all_white_samples = np.array(white_samples)
+
+    var_within_black = np.average(np.var(all_black_samples, axis=0))
+    var_within_white = np.average(np.var(all_white_samples, axis=0))
+
+
+    avg_black = np.average(all_black_samples, axis=0)
+    avg_white = np.average(all_white_samples, axis=0)
+
+    var_between_avg_black_and_white = np.average(np.var(np.array([avg_black, avg_white]), axis=0))
+
+    # Check that there is a significant difference bet
+    too_much_black_var = False
+    too_much_white_var = False
+
+    if (
+        (var_within_black > max_relative_variance_rel_to_b_w_diff * var_between_avg_black_and_white) or
+        (var_within_white > max_relative_variance_rel_to_b_w_diff * var_between_avg_black_and_white)
+        ):
+        raise CalibrationException("Variance among calibration images is high compared to variance between average black image and average white image. Var within black calibration images: {} within white calibration images: {}. Var between average black and average white image: {}. Is the DMD on? When displaying a white image, is the projected image overlapping the area captured by the camera sensor? Is the exposure time long enough?".format(
+            var_within_black,
+            var_within_white,
+            var_between_avg_black_and_white,
+        ))
+
+    # Just call the average pixel value in black images the black level
+    black_level = np.average(avg_black)
+
+    # In addition to checking the variance of black images relative to each other, make sure the average black image is more or less even.
+    var_in_avg_black_img = np.var(avg_black)
+    if var_in_avg_black_img > max_black_field_var_rel_to_b_w_diff * var_between_avg_black_and_white:
+        raise CalibrationException("The unevenness of the average black field is too high: variance within avg black calibration image is {}, compared to variance between average black and average white image: {}.".format(
+            var_in_avg_black_img,
+            var_between_avg_black_and_white,
+        ))
+
+    var_in_avg_white_img = np.var(avg_white)
+    if var_in_avg_white_img > max_white_field_var_rel_to_b_w_diff * var_between_avg_black_and_white:
+        raise CalibrationException("The unevenness of the average white field is too high: variance within avg white calibration image is {}, compared to variance between average black and average white image: {}.".format(
+            var_in_avg_white_img,
+            var_between_avg_black_and_white,
+        ))
+
+
+
+
+
+
+
+
+    # all_black_samples -
+
+    # black_sigma = np.sqrt()
+
+
+    # avg_diff = avg_white - avg_black
+
+
+
+
+
+
+    # @TODO account for feh latency?
+
+def calibrate(core, raspi_controller, workdir):
+    dmd_img_dir = os.path.join(workdir, "imgsfordmd")
+    os.makedirs(dmd_img_dir, exist_ok=True)
+
+    get_background_and_white_images(core, raspi_controller, workdir)
 
     dmd_h, dmd_w = DMD_H_W
 
-    # edge_margin = 10
     circle_diameter = 19
     edge_margin = math.ceil(circle_diameter / 2)
-    
-    grid_x_positions = np.arange(edge_margin, dmd_w - 1 - edge_margin)
-    grid_y_positions = np.arange(edge_margin, dmd_h - 1 - edge_margin)
 
-    for dmd_center_y in grid_y_positions:
-        for dmd_center_x in grid_x_positions:
+    grid_x_positions = np.arange(edge_margin, dmd_w - 1 - edge_margin, 70)
+    grid_y_positions = np.arange(edge_margin, dmd_h - 1 - edge_margin, 70)
+
+    for circle_dmd_y in grid_y_positions:
+        for circle_dmdx in grid_x_positions:
             marker_img_dmd = np.zeros(DMD_H_W, dtype=float)
-            
+
+            draw_white_circle(marker_img_dmd, circle_diameter, circle_dmd_x, circle_dmd_y)
+
+            circle_path = os.path.join(dmd_img_dir, "circle_calibration_marker.tiff")
+            save_img_for_dmd(marker_img_dmd, circle_path)
+            raspi_controller.send_and_show_image_on_dmd(circle_path)
+
 
 
     # raspi_controller.send_and_show_image_on_dmd
@@ -68,5 +196,10 @@ def calibrate(hostname, username, password, pi_interactive_script_path, workdir)
         core = bridge.get_core()
         raspi_controller = RaspiController(hostname, username, password, pi_interactive_script_path)
 
-        calibrate(core, raspi_controller, workdir)
+        try:
+            calibrate(core, raspi_controller, workdir)
+        except:
+            raspi_controller.stop_showing_image_on_dmd()
+            raise
+        raspi_controller.stop_showing_image_on_dmd()
 
--- desktop/src/camera.py
+++ desktop/src/camera.py
@@ -2,8 +2,13 @@ import pycromanager
 import numpy as np
 
 
-def take_micromanager_pic(core):
+def take_micromanager_pic_as_float_arr(core):
     core.snap_image()
     tagged_image = core.get_tagged_image()
     pixels = np.reshape(tagged_image.pix, newshape=[tagged_image.tags['Height'], tagged_image.tags['Width']])
+
+    assert pixels.dtype == np.uint16, "Expecting the raw image from micromanager to be 16 bit integer format"
+
+
+
     return pixels
