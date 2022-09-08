be0a6a2658f6f0f9ee949ff407595850b123bd0e
Paul Kreymborg <frenebo@gmail.com>
2022-09-08 09:38:58 -0700

    writing code to find blobs in photos

--- desktop/src/calibrate.py
+++ desktop/src/calibrate.py
@@ -4,6 +4,7 @@ import tifffile
 import pycromanager
 import math
 import os
+import cv2
 
 from .raspiinterface import RaspiController
 from .camera import take_micromanager_pic
@@ -58,7 +59,7 @@ def draw_white_circle(np_float_img, diameter, center_x, center_y):
     np_float_img[np_float_img > 1.0] = 1.0
 
 
-def get_background_and_white_images(core, raspi_controller, workdir):
+def get_background_black_and_white_levels(core, raspi_controller, workdir):
     # @TODO calibrate these values
     black_samples = 10
     white_samples = 10
@@ -77,10 +78,6 @@ def get_background_and_white_images(core, raspi_controller, workdir):
     for i in range(black_samples):
         black_img_microphoto = take_micromanager_pic_as_float_arr(core)
         black_photos.append(black_img_microphoto)
-
-
-
-    # avg_black = np.average()
 
 
     white_img_dmd = np.ones(DMD_H_W, dtype=float)
@@ -119,9 +116,6 @@ def get_background_and_white_images(core, raspi_controller, workdir):
             var_between_avg_black_and_white,
         ))
 
-    # Just call the average pixel value in black images the black level
-    black_level = np.average(avg_black)
-
     # In addition to checking the variance of black images relative to each other, make sure the average black image is more or less even.
     var_in_avg_black_img = np.var(avg_black)
     if var_in_avg_black_img > max_black_field_var_rel_to_b_w_diff * var_between_avg_black_and_white:
@@ -138,31 +132,122 @@ def get_background_and_white_images(core, raspi_controller, workdir):
         ))
 
 
+    # Just call the average pixel value in black images the black level
+    black_level = np.average(avg_black)
+    # And average pixel value in white images the white level
+    white_level = np.average(avg_white)
+
+    return black_level, white_level
+
+
+def find_blobs_in_photo(np_float_img, black_level, white_level):
+    gaussian_blur_sigma = 7
+    touching_edge_pixels_threshold = 3
+
+    # Areas closer to white than to black are considered white
+    thresh = (black_level + white_level) / 2
+
+
+    blurred = cv2.GaussianBlur(img, (gaussian_blur_sigma, gaussian_blur_sigma), 0)
+
+    above_thresh = np.uint8(blurred > thresh)
+
+    contours, hierarchy = cv2.findContours(above_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # if len(contours) == 0:
+
+    # if len(contours) > 1:
+
+    found_blobs = []
+
+
+    for contour_i in range(len(contours)):
+        contour = contours[contour_i]
+        area = cv2.contourArea(contour)
+        perimeter = cv2.arcLength(contour,True)
+
+
+        circularity = 2*np.sqrt(np.pi*area)  / perimeter
+
+        x_min,y_min,width,height = cv2.boundingRect(cntr)
+
+        is_touching_edge = (
+            (x_min <= touching_edge_pixels_threshold) or
+            (y_min <= touching_edge_pixels_threshold) or
+            (x_min + width >= np_float_img.shape[0] - touching_edge_pixels_threshold) or
+            (y_min + height >= np.float_img.shape[1] - touching_edge_pixels_threshold)
+            )
+
+        M = cv2.moments(big_contour)
+        cx = m["m10"]/M["m00"]
+        cy = M["m01"]/M["m00"]
+
+
+        found_blobs.append({
+            "touching_edge": is_touching_edge,
+            "circularity": circularity,
+            "perimeter": perimeter,
+            "area": area,
+            "centroid_x": cx,
+            "centroid_y": cy,
+            "x_min": x_min,
+            "y_min": y_min,
+            "width": width,
+            "height": height,
+        })
+
+    return found_blobs
 
 
 
+def connected_components_old_find_blobs_in_photo(np_float_img, black_level, white_level):
+    gaussian_blur_sigma = 7
+    touching_edge_pixels_threshold = 3
 
+    # Areas closer to white than to black are considered white
+    thresh = (black_level + white_level) / 2
 
+    blurred = cv2.GaussianBlur(img,(gaussian_blur_sigma, gaussian_blur_sigma),0)
 
-    # all_black_samples -
+    above_thresh = np.uint8(blurred > thresh)
 
-    # black_sigma = np.sqrt()
+    output = cv2.connectedComponentsWithStats(above_thresh)
 
+    numLabels, labels, stats, centroids = output
 
-    # avg_diff = avg_white - avg_black
+    found_blobs = []
 
+    for label in range(numLabels):
+        # If this blob corresponds to a dark region
+        if np.any((labels == 0) == np.logical_not(above_thresh)):
+            continue
+        # If this blob corresponds to a bright region, then continue to
+        x_min, y_min, width, height, area = stats[label]
+        centroid_x, centroid_y = centroids[label]
 
+        # If blob is within the threshold distance of the edge of the image, consider it touching edge
+        touching_edge = (
+            (x_min <= touching_edge_pixels_threshold) or
+            (y_min <= touching_edge_pixels_threshold) or
+            (x_min + width >= np_float_img.shape[0] - touching_edge_pixels_threshold) or
+            (y_min + height >= np.float_img.shape[1] - touching_edge_pixels_threshold)
+            )
 
+        found_blobs.append({
+            "centroid_x": centroid_x,
+            "centroid_y": centroid_y,
+            "area": area,
+            "touching_edge": touching_edge,
+        })
 
+    return found_blobs
 
-
-    # @TODO account for feh latency?
 
 def calibrate(core, raspi_controller, workdir):
     dmd_img_dir = os.path.join(workdir, "imgsfordmd")
     os.makedirs(dmd_img_dir, exist_ok=True)
 
-    get_background_and_white_images(core, raspi_controller, workdir)
+    black_level, white_level = get_background_black_and_white_levels(core, raspi_controller, workdir)
 
     dmd_h, dmd_w = DMD_H_W
 
@@ -173,7 +258,7 @@ def calibrate(core, raspi_controller, workdir):
     grid_y_positions = np.arange(edge_margin, dmd_h - 1 - edge_margin, 70)
 
     for circle_dmd_y in grid_y_positions:
-        for circle_dmdx in grid_x_positions:
+        for circle_dmd_x in grid_x_positions:
             marker_img_dmd = np.zeros(DMD_H_W, dtype=float)
 
             draw_white_circle(marker_img_dmd, circle_diameter, circle_dmd_x, circle_dmd_y)
@@ -182,7 +267,13 @@ def calibrate(core, raspi_controller, workdir):
             save_img_for_dmd(marker_img_dmd, circle_path)
             raspi_controller.send_and_show_image_on_dmd(circle_path)
 
+            circle_microphoto = take_micromanager_pic_as_float_arr(core)
 
+            found_cam_circles = find_circles_in_photo(circle_microphoto, black_level, white_level)
+
+
+
+    # @TODO account for feh latency?
 
     # raspi_controller.send_and_show_image_on_dmd
     # @TODO : ignore points that are less than half a radius from an edge of the camera field
